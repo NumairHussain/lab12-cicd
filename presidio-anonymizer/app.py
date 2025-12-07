81aa2b266bfdd27fef3028963bf578410955f076
Numair Hussain <123851476+NumairHussain@users.noreply.github.com>
2025-12-07 21:31:29 +0000

    HOPEFULLY FIXED LINTING

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -8,9 +8,10 @@ from pathlib import Path
 from flask import Flask, Response, jsonify, request
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError, OperatorConfig
-from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
+from presidio_anonymizer.services.app_entities_convertor import (
+    AppEntitiesConvertor,
+)
 from werkzeug.exceptions import BadRequest, HTTPException
-
 
 DEFAULT_PORT = "3000"
 
